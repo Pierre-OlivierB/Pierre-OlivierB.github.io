e6cfd518ab11e2819ba4e9ee84dd6b6c340264b9
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-11-28 11:16:43 +0100

    add imgs for articles and add 2sd and 3thd 1/2 article

--- monSiteCda/js/main_vei.js
+++ monSiteCda/js/main_vei.js
@@ -63,6 +63,115 @@ const article_1 = `
               <b>Materialize</b> gives really good feedback experiences'.</p>
       </article>
 `;
+const article_2 = `
+<h1>Three.js</h1>
+      <article class="" id="article-presentation">
+          <h3>29/11/2021</h3>
+          <p>To begin a strong and fast project you can use vite.js but it needs vue.js.
+          By this way you can import easily and have a render without recharging the page.
+          To resume, vite is a local server for developers. <br> <br>
+              <b>First of all, what are the basics of Three.js? </b><br></p>
+
+          <p> 
+          <b>Based on :</b> Javascript <br>
+          <b>What that :</b> it's a langage on object.</br>
+              <b>What for ?</b> To build a scene.</br>
+              <b>How ?</b> There are three important's ways to keep in mind : a scene, a camera and a render.</p>
+          <p> <b>Ok, now we can begin :</b> </br>Second : How to use it ? <br>
+              <b>Download</b>
+              OR
+              <b>Compile</b> .</p>
+          <p><b>Download :</b></br>
+          No download, or just copy on an other js page and css page that you need.</br></br>
+              <b>Compiled :</b></br>
+              On your own js page.
+                      </br>
+                      <br>
+                      <b>For beginners be carefull :</b> <br>
+                      Be careful of imports with vite.js. For easy way, the best is to let auto file to complete your call. With this you will understand what and when you add functions. But without this aim's function your scene will fail.
+
+          </p>
+          <p>Is it ok ? <br> Now we can finally use it ! <br> <br> <b>To understand the structure of this framework :</b></p>
+             <p>
+              With this you can create a movie : <br> Take for example Mr.Cube.<br> Mr.Cube wants to be the new famous actor. So he takes his faith on his own and go to meet the great compagny near his home : <strong>three.js.</strong> <br>
+              Mr. Cube takes his car and reaches his goal in whistling.
+              When he finally arrives at the door of the company an  intense light hurt his eyes. He has pain to walk at the door. <br>
+              <b>He knocks.</b> <br>
+              A long wait next this. No response.
+              Mr Cube decedes to enter without permissions.
+              He enters on the scene. He is afraid. There is no sound and no light. <br>
+              <b>“Anyone here?”</b> he said...</p>
+              <img src="./img/mrcube_blackscene.png" alt="black scene render with three js">
+              
+          <p>Despite the little light he sees cameras, he feels the scene at his foot and can imagine the render of this situation.
+          But no one and nothing. <br><br>
+          
+          <b>“ANYONE THERE?”</b> scream it.<br>
+          <b>“To the light.”</b> Said a voice get out of nowhere.<br>
+          Cube, transform of really vegetable and try to say: <b>“It's my best profile”</b> does he stammer.</p>
+          <img src="./img/mrcube_firstlight.png" alt="cube apparition in black scene render with three js">
+          <p><b>“Have no fear...”</b> said the far far away voice.<br>
+          <b>“Turn around, we must see you in all aspects!”</b></p>
+          <img src="./img/mrcube_firststeps.png" alt="3D cube in black scene render with three js">
+          <p><b> “Thanks Mr Cube!”</b> said the voice. <br>
+          <b>“We can work together.”</b></p>
+          <p> For the end of the story of Mr Cube, the director it's you. <br>
+          So what are you waiting for?<strong> Did it!</strong> </p>
+      </article>
+`;
+
+const article_3 = `
+<h1>Exception (Java) 1/2</h1>
+      <article class="" id="article-presentation">
+          <h3>08/01/2022 part. 1</h3>
+          <p>In practising of code you must have met this red message! Its name is Exception. It has a lot of appearance but you need to understand why Exception appear to fully catch information throws to you by the computer.<br>Ok now we can begin: <br> <br>
+              <b>First of all : </b><br></p>
+          <p><b>How to keep calm?</b> Breath and read calmly all information throw at you. <br>
+          <b>How to use?</b> It's written just after that.</br></p>
+          <p>Is it ok ? <br> Now we can finally use it ! <br> <br> <b>First step and first example.</b></p>
+          <p>Take a code where it asks for birthday.
+          You can write a birthday like what's you have been asking for or you can do like Paul and writes his name on the sentence.</p>
+          <img src="./img/premier_test.jpg" alt="first test">
+          <p>Yes Paul, it's your first red message. Keep calm, more will appear soon. <br>
+          So, to understand this message:</p>
+          <p>The Exception name is :“InputMismatchException” <br>
+          Program wait an “int” but when the program reads the input return it's a String “Paul”. <br><br>
+          <b>First possibility to do something with this:</b><br>
+          Just leave exceptions without more focus.<br>
+          You just have to write  “throws Exception” at the end of your method.<br>
+          Traduction of this is : It's possible to throw exception, but it's ok.<br>
+          You see Paul, it's not a big deal.</p>
+          <p><b>Second Possibility : trick or treat?</b></p>
+          <p>Delete the last words written and continue.<br>
+          So let's try with Try and Catch.<br>
+          These two are bigs and strongs.<br>
+          Try throws instructions.<br>
+          And when it's time to interact, if Java meet something wrong, Catch appeares and Java gives instructions to Catch when catch match with the message deliver. At his turn, Catch throws instruction.</p>
+          <p>First step : <br>
+          Add the exception throws. We will catch you red,  at the top!<br>
+          <strong>import java.util.InputMismatchException;</strong> <br>
+          Then in the Catch we can write his name: <b> InputMismatchException</b>, caution doesn't forget “e” or just something for you with a precise name. <br> This gives to us:</p>
+          <img src="./img/screen_e_print.jpg" alt="print exception">
+          <p>Look Paul. <br>All lines disappear.<br> Just one, we throw it to alert users.<br>
+          Like in Else if the program is stopped.<br><br>
+          To catch a lot of exceptions, we can use more catch.</p>
+          <img src="./img/exemple_multiple_catch.jpg" alt="exceptions with multiple catch">
+          <p>By convention, we organise catch from more probabilities to less.</p>
+          <p><b>Third Possibility: meet his mother</b><br>
+          Exceptions have a mother class “Exception”. She is the last catch possible. To fully catch all the family if you don't want to treat precisely an Exception child, you have to call her at the final catch.<br>
+          Like this:</p>
+          <img src="./img/mother_class_exception.jpg" alt="mother class exception">
+          <p>She scares me. And you Paul... Paul?</p>
+          <p><b>Fourth Possibility: finally!</b><br>
+          If you just want to share a message with user no matter what all throws you can use “finally” to be sure to throw instruction to the user.<br>
+          This looks like:</p>
+          <img src="./img/finally.jpg" alt="finally exception part">
+          <p>And this if you run with it:</p>
+          <img src="./img/test_finally.jpg" alt="test finally exception part">
+          <p>Yeah, I know, it's cool.<br>
+          Look at this Paul!</p>
+      </article>
+`;
 
 let articles = document.querySelectorAll(".article");
 function article() {
@@ -70,13 +179,18 @@ function article() {
 }
 articles.forEach((article) => {
   article.addEventListener("click", (e) => {
-    console.log(e.path[1].id);
+    // console.log(e.path[1].id);
     let content = e.path[1].id;
     switch (content) {
       case "article_1":
         presentation.innerHTML = article_1;
         break;
-
+      case "article_2":
+        presentation.innerHTML = article_2;
+        break;
+      case "article_3":
+        presentation.innerHTML = article_3;
+        break;
       default:
         break;
     }
